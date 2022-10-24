831c347d324b04e97d23648b83fc47905c0b0ae4
Vica_Rom <vicarom806@gmail.com>
2022-10-24 18:16:24 +0300

    Задача Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int sqr2 = number2*number2;
+
+if (number1 == sqr2)
+{
+    Console.Write("Да");
+}
+else 
+{
+    Console.Write("Нет");
+}
