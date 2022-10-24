5aa04c057f6532d17717befec9a0930c6dc405dd
Vica_Rom <vicarom806@gmail.com>
2022-10-24 18:59:50 +0300

    Внесли правки в задачу 3: указали вывод если введен номер 0 или больше 7 в указании дня недели

---
+++ Task 2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("Введите номер дня недели ");
+int DayNumber = int.Parse(Console.ReadLine());
+
+if (DayNumber == 1)
+{
+    Console.Write("Понедельник");
+}
+if (DayNumber == 2)
+{
+    Console.Write("Вторник");
+}
+if (DayNumber == 3)
+{
+    Console.Write("Среда");
+}
+if (DayNumber == 4)
+{
+    Console.Write("Четверг");
+}
+if (DayNumber == 5)
+{
+    Console.Write("Пятница");
+}
+if (DayNumber == 6)
+{
+    Console.Write("Суббота");
+}
+if (DayNumber == 7)
+{
+    Console.Write("Воскресенье");
+}
+if (DayNumber>7||DayNumber==0)
+{
+    Console.Write("Введен неверный номер");
+}
