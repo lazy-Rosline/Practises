59cc789acdf01118c39827516dd09b1ef1a5b8bb
Vica_Rom <vicarom806@gmail.com>
2022-10-27 20:18:01 +0300

    Task 14 кратность 7 И 23 готово

---
+++ Task 14/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());  
+if(N%7==0&&N%23==0) 
+{
+    Console.Write("-->да");
+}
+else 
+{
+    Console.Write("нет");
+}
