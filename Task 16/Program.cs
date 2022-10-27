65508d0590fa1908ad9f92e883073880187f4bf4
Vica_Rom <vicarom806@gmail.com>
2022-10-27 20:36:10 +0300

    Task 16 b квадрат а готово

---
+++ Task 16/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Первое число а=: ");
+int a = int.Parse(Console.ReadLine());  
+Console.Write("Второе число b=: ");
+int b = int.Parse(Console.ReadLine());
+if(a==b*b) 
+{
+    Console.Write("a квадрат b");
+}
+else 
+{
+    if(b==Math.Pow(a,2)) 
+    {
+        Console.Write("b квадрат а");
+    }
+    else 
+    {
+        Console.Write("не является квадратом");
+    }
+}
