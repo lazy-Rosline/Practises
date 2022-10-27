8671c48359a79a388543c369b6d46ccf5db20440
Vica_Rom <vicarom806@gmail.com>
2022-10-27 20:09:33 +0300

    Task 12 checked

---
+++ Task 12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int N1 = int.Parse(Console.ReadLine());  
+Console.Write("Введите второе число: ");
+int N2 = int.Parse(Console.ReadLine());
+if(N1%N2==0) 
+{
+    Console.Write("-->кратно");
+}
+else 
+{
+    Console.Write($"--> не кратно, остаток {N1%N2}");
+}
