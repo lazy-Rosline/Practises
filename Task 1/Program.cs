010949bf061e1b4b5a1b95fe5e49bfdbb2c836d9
Vica_Rom <vicarom806@gmail.com>
2022-10-28 11:50:33 +0300

    renamed folder Task 1 correctly

---
+++ Task 1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число A: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число B: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 == number2*number2)
+{
+    Console.Write("А является квадратом В");
+}
+else 
+{
+    Console.Write("А не является квадратом В");
+}
