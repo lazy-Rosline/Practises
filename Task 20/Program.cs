27489c3a4f0ab36ad91b1792f152d5392ecd37d5
Vica_Rom <vicarom806@gmail.com>
2022-10-28 09:08:57 +0300

    Task 20

---
+++ Task 20/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите координату х точки А ");
+int x1 = int.Parse(ReadLine());
+Write("Введите координату y точки А ");
+int y1 = int.Parse(ReadLine());
+Write("Введите координату х точки B ");
+int x2 = int.Parse(ReadLine());
+Write("Введите координату y точки B ");
+int y2 = int.Parse(ReadLine());
+double result = Math.Pow(((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)),0.5);
+Write($"длина отрезка {result:f3}");
