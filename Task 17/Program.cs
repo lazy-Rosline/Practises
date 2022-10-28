ba0d8ffe1de2288bebd34872117471724ed9a35d
Vica_Rom <vicarom806@gmail.com>
2022-10-28 08:27:23 +0300

    Task 17

---
+++ Task 17/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите координату х: ");
+int x = int.Parse(Console.ReadLine());  
+Write("Введите координату y: ");
+int y = int.Parse(Console.ReadLine()); 
+if(x>0&&y>0) 
+{
+    Console.Write("1 четверть");
+}
+if(x>0&&y<0) 
+{
+    Console.Write("4 четверть");
+}
+if(x<0&&y>0) 
+{
+    Console.Write("2 четверть");
+}
+if(x<0&&y<0) 
+{
+    Console.Write("3 четверть");
+}
