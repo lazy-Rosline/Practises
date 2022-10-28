a968e0c5c49ffc37cd806ff7254028630a618204
Vica_Rom <vicarom806@gmail.com>
2022-10-28 11:31:02 +0300

    added Task 6

---
+++ Task 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Ваше число?: ");
+int a=int.Parse(Console.ReadLine());
+
+if(a%2==0)
+{
+    Console.Write("четное");
+}
+else
+{
+    Console.Write("нечетное");
+}
