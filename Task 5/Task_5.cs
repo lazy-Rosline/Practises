453e13fe3e1eadd0e94199dfe6ebd00f15973cc4
Vica_Rom <vicarom806@gmail.com>
2022-10-28 11:27:27 +0300

    Task 3 renamed to Task 5

---
+++ Task 5/Task_5.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine());
+int count = -N;
+while (count<=N)
+{
+    Console.Write($"{count} ");
+    count++;
+}
+
