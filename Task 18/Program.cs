1f86d0f7ee6af4040a82aac93b8794c0231f6b92
Vica_Rom <vicarom806@gmail.com>
2022-10-28 08:32:42 +0300

    Task 18

--- Task 18/Program.cs
+++ Task 18/Program.cs
@@ -6,17 +6,17 @@ Write("Введите номер четверти: ");
 int number = int.Parse(Console.ReadLine());  
 if(number==1) 
 {
-    Console.WriteLine("--> x>0; y>0");
+    WriteLine("--> x>0; y>0");
 }
 if(number==2) 
 {
-    Console.WriteLine("--> x<0; y>0");
+    WriteLine("--> x<0; y>0");
 }
 if(number==3) 
 {
-    Console.WriteLine("--> x<0; y<0");
+    WriteLine("--> x<0; y<0");
 }
 if(number==4) 
 {
-    Console.WriteLine("--> x>0; y<0");
+    WriteLine("--> x>0; y<0");
 }
