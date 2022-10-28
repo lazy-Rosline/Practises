2d229d714939772b8694a42dc41dc3c336176134
Vica_Rom <vicarom806@gmail.com>
2022-10-28 10:47:46 +0300

    Task 19

---
+++ Task 19/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите пятизначное число: ");
+int number = int.Parse(Console.ReadLine()); 
+int a = number/1000;
+int b = number%100; 
+if((a/10==b%10)&&(a%10==b/10)) 
+{
+    Write("да");
+}
+else 
+{
+    Write("нет");
+}
