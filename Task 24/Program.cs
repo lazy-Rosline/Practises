037e68c60de42c9c28cb35cbcf9c494d18cb0a42
Vica_Rom <vicarom806@gmail.com>
2022-11-02 19:02:31 +0300

    Task 24 added

---
+++ Task 24/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+int sum=0;
+for (int i=1;i<=N;i++)
+{
+    sum=sum+i;
+}
+Write($"сумма от 1 до N = {sum}");
