0450cb1162dbbe9fe764948bb4cec72f607d6297
Vica_Rom <vicarom806@gmail.com>
2022-11-04 18:00:38 +0300

    Решили пример 13 через обращение как к строчному символу к каждой цифре числа...

---
+++ Task 13/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите число: ");
+string s = Console.ReadLine();
+int length = s.Length;
+  
+//if (s[2]==null)
+if (length<3)
+{
+    Console.Write("третьей цифры нет");
+}
+else {
+    Console.Write(s[2]);
+}
