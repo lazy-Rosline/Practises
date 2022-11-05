5f1557cdb9a7aee1cb25616e4e1f7214157cf9a9
Vica_Rom <vicarom806@gmail.com>
2022-11-05 19:55:30 +0300

    Task 26 finished, 2 ways

---
+++ Task 26/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int N=int.Parse(Console.ReadLine());
+int NumberAmount=GetNumbers(N);
+WriteLine($"кол-во цифр равно {NumberAmount}");
+
+int GetNumbers (int Initial)
+{
+    int count=0;
+    while (Initial>0)
+    {
+        count+=1;
+        Initial/=10;
+    }
+    return count;
+}
+//МОЕ решение:
+// int i=10;
+// int result=1;
+// while (N>i)
+// {
+//     i*=10;
+//     result++;
+// }
+// Write(result);
