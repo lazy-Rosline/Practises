bbc2e02a1844943e86dcc7bfb0120debd1047822
Vica_Rom <vicarom806@gmail.com>
2022-11-05 22:53:34 +0300

    Task 27 done

---
+++ Task 27/Program.cs
@@ -0,0 +1,19 @@
+﻿// выдаёт сумму цифр в числе
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+
+int SumOfFigures(int SetN)
+{
+    int count=0;
+    while (SetN>0)
+    {
+        count=count+SetN%10;
+        SetN/=10;
+    }
+    return count;
+}
+
+Write(SumOfFigures(int.Parse(Console.ReadLine())));
