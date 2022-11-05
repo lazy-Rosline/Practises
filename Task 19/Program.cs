1e42bd1a95f57a4f162c0b0a2ab55eea40bcaa9d
Vica_Rom <vicarom806@gmail.com>
2022-11-05 20:54:47 +0300

    Task 19 2nd method

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -3,10 +3,11 @@ using static System.Console;
 
 Clear();
 Write("Введите пятизначное число: ");
-int number = int.Parse(Console.ReadLine()); 
-int a = number/1000;
-int b = number%100; 
-if((a/10==b%10)&&(a%10==b/10)) 
+int N = int.Parse(Console.ReadLine()); 
+// int a = number/1000;
+// int b = number%100; 
+// if((a/10==b%10)&&(a%10==b/10)) 
+if((N/10000==N%10)&&(N/1000%10==N%100/10))
 {
     Write("да");
 }
