a8bfb654151d9005c7539a2695db67832120ea3c
Vica_Rom <vicarom806@gmail.com>
2022-11-05 20:18:09 +0300

    Task 28 checked

---
+++ Task 28/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+Write(Factorial(int.Parse(ReadLine())));
+
+double Factorial (int x)
+{   
+    double result=1;
+    for(int i=1;i<=x;i++)
+    {
+       result*=i;
+    }
+    return result;
+}
+
+
+
