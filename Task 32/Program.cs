9b17dec3af80e4461d75ef8acb0cc844469cfb7e
Vica_Rom <vicarom806@gmail.com>
2022-11-06 11:02:45 +0300

    Task 32 (2 ways)

---
+++ Task 32/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] ArrayTurnOpposite(int[] arr)
+{
+    for(int i=0;i<arr.Length;i++)
+    {
+        arr[i]=-arr[i];
+    }
+    return arr;
+}
+
+void Inverse(int[] array)
+{
+    for(int i=0;i<array.Length;i++)
+    {
+        Write($"{-array[i]} ");
+    }
+}
+
+int[] array={1,2,3,4,8,-9,18};
+// array= ArrayTurnOpposite(array);
+// WriteLine(String.Join(",", array));
+Inverse(array);
+
