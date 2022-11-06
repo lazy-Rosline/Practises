65e07c0b12655fafc1ced682f2e820ee10344c2e
Vica_Rom <vicarom806@gmail.com>
2022-11-06 21:36:50 +0300

    Task 35 done

---
+++ Task 35/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] result=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        result[i]=new Random().Next(min, max+1);
+    }
+    return result;
+}
+
+int[] array=GetArray(123,0,100);
+WriteLine(String.Join(", ",array));
+int count=0;
+for (int i=0;i<array.Length;i++)
+{
+    if((array[i]>=10)&&(array[i]<=99)) 
+    {
+        count+=1;
+    }
+}
+Write(count);
