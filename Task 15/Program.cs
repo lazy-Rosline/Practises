8784cc398b10a4d7ff633891e6099f177bb53094
Vica_Rom <vicarom806@gmail.com>
2022-11-06 09:40:46 +0300

    Task 15

---
+++ Task 15/Program.cs
@@ -0,0 +1,24 @@
+﻿// проверяет по номеру является ли день недели выходным
+using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите номер дня недели ");
+int dayNumber=int.Parse(ReadLine());
+switch(dayNumber)
+{
+    case 6:
+    case 7:
+    {
+        Write("выходной");
+        break;
+    }
+    default:
+    {
+        Write("не выходной");
+        break;
+    }
+
+
+}
