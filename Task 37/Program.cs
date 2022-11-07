42f3621ce5f442d76eb45514e55a2631979622e3
Vica_Rom <vicarom806@gmail.com>
2022-11-07 12:51:54 +0300

    Task 37

---
+++ Task 37/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] newArray(int size)
+{
+    int[] array=new int[size];
+    for(int index=0;index<size;index++)
+    {
+        array[index]=new Random().Next(0, 11);
+    }
+    WriteLine(String.Join("  ", array));
+    int length = size/2;
+    if(size%2==1) length++;
+    int[] result=new int[length];
+    for(int i=0;i<result.Length;i++)
+    {
+        result[i]=array[i]*array[size-1-i];
+    }
+    if (size%2==1) result[length-1]=array[size/2];
+    return result;
+}
+
+WriteLine("Задайте число элементов массива: ");
+int N = int.Parse(ReadLine());
+int[] Transformed=newArray(N);
+WriteLine(String.Join("  ", Transformed));
