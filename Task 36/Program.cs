e76f1677e080d4f679324b03bec391e309cb9b0d
Vica_Rom <vicarom806@gmail.com>
2022-11-07 14:06:38 +0300

    Task 36 complete

---
+++ Task 36/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] ArrayFromString(string s)
+{
+    string[] User = s.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    int[] result=new int[User.Length];
+    for(int i=0;i<result.Length;i++)
+    {
+        result[i]=int.Parse(User[i]);
+    }
+    return result;
+}
+int EvenPositionSum (int[] InArr)
+{
+    int sum=0;
+    for(int i=0;i<InArr.Length;i+=2)
+    {
+        sum+=InArr[i];
+    }
+    return sum;
+}
+
+Write("Введите набор чисел (через пробел): ");
+int[] array=ArrayFromString(ReadLine());
+int Sum=EvenPositionSum(array);
+WriteLine($"[{String.Join(",",array)}]-->сумма элементов на нечетных позициях: {Sum}");
