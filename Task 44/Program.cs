d4e3009cce98921021db8e7e0f6fbb23ab749863
Vica_Rom <vicarom806@gmail.com>
2022-11-11 20:57:30 +0300

    Task 44

--- Task 44/Program.cs
+++ Task 44/Program.cs
@@ -4,17 +4,17 @@ using static System.Console;
 
 Clear();
 
-int[] Fibonacci2 (int size)
+int[] Fibonacci2(int size)
 {
-    int[] res=new int[size];
-    res[0]=0;
-    res[1]=1;
-    for(int i=2; i<res.Length;i++)
+    int[] res = new int[size];
+    res[0] = 0;
+    res[1] = 1;
+    for (int i = 2; i < res.Length; i++)
     {
-        res[i]=res[i-1]+res[i-2];
+        res[i] = res[i - 1] + res[i - 2];
     }
     return res;
 }
 
 Write("Введите число: ");
-WriteLine(String.Join(" ",Fibonacci2(int.Parse(ReadLine()))));
+WriteLine(String.Join(" ", Fibonacci2(int.Parse(ReadLine()))));
