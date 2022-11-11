980dba0350a7e494eb9e7a1696d64ca66ec9cecf
Vica_Rom <vicarom806@gmail.com>
2022-11-11 21:08:34 +0300

    Task 39

---
+++ Task 39/Program.cs
@@ -0,0 +1,30 @@
+﻿// переворачивает одномерный массив:
+using System;
+using static System.Console;
+
+Clear();
+
+int[] GetArray(int size, int min, int max)
+{
+    int[] res=new int[size];
+    for(int i=0; i<size; i++)
+    {
+        res[i]= new Random().Next(min, max+1);
+    }
+    return res;
+}
+
+int[] arrayReverse(int[] InArray)
+{
+    int[] result=new int[InArray.Length];
+    for(int k=0; k<result.Length; k++)
+    {
+        result[k]=InArray[InArray.Length-1-k];
+    }
+    return result;
+}
+
+int[] array1=GetArray(13, 23, 144);
+WriteLine(String.Join((" "), array1));
+int[] arrayInverse = arrayReverse(array1);
+WriteLine(String.Join((" "), arrayInverse));
