f4991bd5eddcf24cc9b6a18d467269cccebf6c76
Vica_Rom <vicarom806@gmail.com>
2022-11-11 20:42:36 +0300

    Task 42 second variant

--- Task 42/Program.cs
+++ Task 42/Program.cs
@@ -4,28 +4,44 @@ using static System.Console;
 
 Clear();
 
-int[] ConvertToBinary(int number) 
+string DecToBinary(string user)
 {
-    int length=0;
-    int N=number;
-    while (N!=0)
+    int N=int.Parse(user);
+    string res="";
+    while(N>0)
     {
+        int v=N%2;
+        res=v.ToString()+res;
         N=N/2;
-        length++;
     }
-    int[] arrayIn = new int[length];
-    for(int i=0; i<arrayIn.Length;i++)
-    {
-        arrayIn[i]=number%2;
-        number=number/2;
-    }
-    int[] result=new int[length];
-    for(int k=0; k<result.Length; k++)
-    {
-        result[k]=arrayIn[arrayIn.Length-1-k];
-    }
-    return result;
+    return res;
 }
-WriteLine("Введите число: ");
-int x =int.Parse(ReadLine());
-WriteLine(String.Join("",ConvertToBinary(x)));
+
+// int[] ConvertToBinary(int number) 
+// {
+//     int length=0;
+//     int N=number;
+//     while (N!=0)
+//     {
+//         N=N/2;
+//         length++;
+//     }
+//     int[] arrayIn = new int[length];
+//     for(int i=0; i<arrayIn.Length;i++)
+//     {
+//         arrayIn[i]=number%2;
+//         number=number/2;
+//     }
+//     int[] result=new int[length];
+//     for(int k=0; k<result.Length; k++)
+//     {
+//         result[k]=arrayIn[arrayIn.Length-1-k];
+//     }
+//     return result;
+// }
+
+// Write("Введите число: ");
+// int x =int.Parse(ReadLine());
+// WriteLine(String.Join("",ConvertToBinary(x)));
+Write("Введите число: ");
+WriteLine(DecToBinary(ReadLine()));
