07e77c5164700ce816f18701880e06d0160a52f8
Vica_Rom <vicarom806@gmail.com>
2022-11-11 20:57:08 +0300

    Task 44

---
+++ Task 44/Program.cs
@@ -0,0 +1,20 @@
+﻿// выводит N чисел Фибоначчи:
+using System;
+using static System.Console;
+
+Clear();
+
+int[] Fibonacci2 (int size)
+{
+    int[] res=new int[size];
+    res[0]=0;
+    res[1]=1;
+    for(int i=2; i<res.Length;i++)
+    {
+        res[i]=res[i-1]+res[i-2];
+    }
+    return res;
+}
+
+Write("Введите число: ");
+WriteLine(String.Join(" ",Fibonacci2(int.Parse(ReadLine()))));
