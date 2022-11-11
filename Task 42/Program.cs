3f2817f562e69bafe08245a327227934db192d83
Vica_Rom <vicarom806@gmail.com>
2022-11-11 16:29:59 +0300

    Task 42

---
+++ Task 42/Program.cs
@@ -0,0 +1,31 @@
+﻿// переводит в двоичную систему счисления
+using System;
+using static System.Console;
+
+Clear();
+
+int[] ConvertToBinary(int number) 
+{
+    int length=0;
+    int N=number;
+    while (N!=0)
+    {
+        N=N/2;
+        length++;
+    }
+    int[] arrayIn = new int[length];
+    for(int i=0; i<arrayIn.Length;i++)
+    {
+        arrayIn[i]=number%2;
+        number=number/2;
+    }
+    int[] result=new int[length];
+    for(int k=0; k<result.Length; k++)
+    {
+        result[k]=arrayIn[arrayIn.Length-1-k];
+    }
+    return result;
+}
+WriteLine("Введите число: ");
+int x =int.Parse(ReadLine());
+WriteLine(String.Join("",ConvertToBinary(x)));
