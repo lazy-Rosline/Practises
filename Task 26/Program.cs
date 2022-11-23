bdb2c09954f7f30a022bba122642475446bc472c
Vica_Rom <vicarom806@gmail.com>
2022-11-23 08:42:37 +0300

    edits

--- Task 26/Program.cs
+++ Task 26/Program.cs
@@ -4,25 +4,25 @@ using static System.Console;
 Clear();
 Write("Введите число: ");
 int N=int.Parse(Console.ReadLine());
-int NumberAmount=GetNumbers(N);
-WriteLine($"кол-во цифр равно {NumberAmount}");
+// int NumberAmount=GetNumbers(N);
+// WriteLine($"кол-во цифр равно {NumberAmount}");
 //OR - Write(GetNumbers(int.Parse(Console.ReadLine())));
-int GetNumbers (int Initial)
-{
-    int count=0;
-    while (Initial>0)
-    {
-        count+=1;
-        Initial/=10;
-    }
-    return count;
-}
-//МОЕ решение:
-// int i=10;
-// int count=1;
-// while (N>i)
+// int GetNumbers (int Initial)
 // {
-//     i*=10;
-//     count++;
+//     int count=0;
+//     while (Initial>0)
+//     {
+//         count+=1;
+//         Initial/=10;
+//     }
+//     return count;
 // }
-// Write(count);
+//МОЕ решение:
+int i=10;
+int count=1;
+while (N>i)
+{
+    i*=10;
+    count++;
+}
+Write(count);
