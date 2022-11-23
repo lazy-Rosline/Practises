1fa30bf4679040eae0eeee3f9118509960c0e884
Vica_Rom <vicarom806@gmail.com>
2022-11-23 11:00:37 +0300

    Task 29 complete

---
+++ Task 29/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+using System;
+using static System.Console;
+
+Clear();
+
+int[] arr = GetArray(8, 0, 20);
+// PrintArray(arr);
+Write($"[{String.Join(" ", arr)}]");
+
+
+int[] GetArray (int size, int min, int max)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+       Write($"{collection[i]} ");
+    }
+}
