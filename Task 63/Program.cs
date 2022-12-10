09d32092dc01b33892ccd60ae984c7ea95705846
Vica_Rom <vicarom806@gmail.com>
2022-12-10 21:46:15 +0300

    Task 63

---
+++ Task 63/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа выводит числа от 1 до N.
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+Write(RecNumber(1, N));
+
+string RecNumber(int initial, int end)
+{
+    if (initial==end) return initial.ToString();
+    return (initial + " " + RecNumber(initial+1, end));
+}
