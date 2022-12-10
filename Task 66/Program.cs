5a9b99df2d019b8e14111cf1164fd366335ea440
Vica_Rom <vicarom806@gmail.com>
2022-12-10 20:59:32 +0300

    Task 66 еле-еле

---
+++ Task 66/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int M = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int N = int.Parse(ReadLine());
+WriteLine(MNSum(M, N));
+
+int MNSum (int m, int n)
+{
+    if(m==n) return m;
+    else return m+MNSum(m+1, n);
+}
