0e98dd4b8beb18d46ea45514f4257a632b7a4c98
Vica_Rom <vicarom806@gmail.com>
2022-12-10 21:53:47 +0300

    Task 65

---
+++ Task 65/Program.cs
@@ -0,0 +1,17 @@
+﻿//Программа выводит числа в промежутке от M до N.
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите M: ");
+int M = int.Parse(ReadLine());
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+Write(ShowBetween(M, N));
+
+string ShowBetween (int start, int end)
+{
+    if (start==end) return start.ToString();
+    return (start + " " + ShowBetween(start+1, end));
+}
