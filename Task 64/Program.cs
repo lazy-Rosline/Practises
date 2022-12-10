823f6b8cad03e6a7b3faad56c5e42ee77534b2ae
Vica_Rom <vicarom806@gmail.com>
2022-12-10 20:20:08 +0300

    Task 64, done

---
+++ Task 64/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+//в промежутке от N до 1. Выполнить с помощью рекурсии
+
+using System;
+using static System.Console;
+Clear();
+
+Write("Введите N: ");
+int N = int.Parse(ReadLine());
+WriteLine(NumMinus(N));
+
+string NumMinus (int arg)
+{
+    if(arg==1) return "1";
+    else return $"{arg}, " + NumMinus(arg-1);
+}
