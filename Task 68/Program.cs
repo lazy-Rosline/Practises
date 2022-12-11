127e0d92ef2f1376f875acd24d5f01ab09299c6a
Vica_Rom <vicarom806@gmail.com>
2022-12-11 16:35:46 +0300

    Task 68

---
+++ Task 68/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу вычисления функции Аккермана
+using System;
+using static System.Console;
+Clear();
+
+Write("задайте первое число m: ");
+int m = int.Parse(ReadLine());
+Write("задайте второе число n: ");
+int n = int.Parse(ReadLine());
+Write($"--> A(m,n)= {Accerman(m, n)}");
+
+int Accerman (int value1, int value2)
+{
+    if (value1==0) return value2+1;
+    else 
+    {
+        if(value2==0) return Accerman(value1-1, 1);
+        else return Accerman(value1-1, Accerman(value1, value2-1));
+    }
+}
